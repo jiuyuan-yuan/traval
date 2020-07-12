229d1b7c0856f541d88cdc3ff0f278c7970df298
benc <2627898178@qq.com>
2020-07-12 20:42:48 +0800

    轮播完成

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import fastClick from 'fastclick'
 import '@/assets/styles/reset.css'
 import '@/assets/styles/border.css'
 import '@/assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
+Vue.use(VueAwesomeSwiper)
 fastClick.attach(document.body)
 Vue.config.productionTip = false
 
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList"
+                    :key="item.id">
+        <img :src="item.url"
+             class="swiper-img">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"
+           slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '001',
+        url: require('../../../assets/images/1.jpg')
+      }, {
+        id: '002',
+        url: require('../../../assets/images/2.jpeg')
+      }, {
+        id: '003',
+        url: require('../../../assets/images/3.jpeg')
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.swiper-img {
+  width: 100%;
+}
+.wrapper {
+  width: 100%;
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 31.25%;
+}
+</style>
