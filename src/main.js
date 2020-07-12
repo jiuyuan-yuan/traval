65e0d6ba38608bc8451aba60b5e962be58a447bb
benc <2627898178@qq.com>
2020-07-12 18:48:20 +0800

    初始化C

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import fastClick from 'fastclick'
+import '@/assets/styles/reset.css'
+import '@/assets/styles/border.css'
+import '@/assets/styles/iconfont.css'
 
+fastClick.attach(document.body)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <home-header></home-header>
+    <div class="home">home</div>
+    <router-link to="/list">列表页</router-link>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+export default {
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.home {
+  font-size: 30px;
+}
+</style>
