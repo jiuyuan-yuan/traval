fb43ca0fb544e90f09505eac0bb52d4fa11311ab
benc <2627898178@qq.com>
2020-07-13 12:48:13 +0800

    方框

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -8,7 +8,7 @@
       输入城市/景点</div>
     <div class="header-right">
       城市
-      <sapn class="iconfont arrow-icon">&#xe62a;</sapn>
+      <span class="iconfont arrow-icon">&#xe62a;</span>
     </div>
   </div>
 </template>
---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page,index) of pages"
+                    :key="index">
+        <div class="icon"
+             v-for="item of page"
+             :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content"
+                 :src="item.url">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      iconList: [{
+        id: '001',
+        url: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '热门景点'
+      }, {
+        id: '002',
+        url: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      }, {
+        id: '003',
+        url: 'http://img1.qunarzz.com/piao/fusion/1803/89/55083b0f1951f302.png',
+        desc: '两江夜游'
+      }, {
+        id: '004',
+        url: 'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+        desc: '神秘武隆'
+      }, {
+        id: '005',
+        url: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/338c5b924c5809e8c7b14f60a953c3e2.png',
+        desc: '欢乐谷'
+      }, {
+        id: '006',
+        url: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/b71c1387a921ccf9c6edc7e8def3da90.png',
+        desc: '动物园景点'
+      }, {
+        id: '007',
+        url: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '热门景点'
+      }, {
+        id: '008',
+        url: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/b71c1387a921ccf9c6edc7e8def3da90.png',
+        desc: '动物园景点'
+      }, {
+        id: '009',
+        url: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '热门景点'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../../assets/styles/varibles.styl';
+@import '../../../assets/styles/mixins.styl';
+
+.icons >>> .swiper-container {
+  height: 0;
+  padding-bottom: 50%;
+}
+
+.icon {
+  overflow: hidden;
+  float: left;
+  width: 25%;
+  padding-bottom: 25%;
+  position: relative;
+  height: 0;
+
+  .icon-img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0.44rem;
+    box-sizing: border-box;
+    padding: 0.1rem;
+
+    .icon-img-content {
+      height: 100%;
+      display: block;
+      margin: 0 auto;
+      height: 100%;
+    }
+  }
+
+  .icon-desc {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    line-height: 0.44rem;
+    height: 0.44rem;
+    color: darckTextColor;
+    text-align: center;
+    ellipsis();
+  }
+}
+</style>
